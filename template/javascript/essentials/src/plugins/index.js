59b1e3f746f569933dc0808a7b658cef990ccfb3
JugurthaK <jugurthak@protonmail.com>
2022-11-12 08:51:09 +0100

    app parameter added to essentials registerPlugins method

--- template/javascript/essentials/src/plugins/index.js
+++ template/javascript/essentials/src/plugins/index.js
@@ -10,7 +10,7 @@ import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 
-export function registerPlugins () {
+export function registerPlugins (app) {
   loadFonts()
   app
     .use(vuetify)
