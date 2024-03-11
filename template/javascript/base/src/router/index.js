3959c623029260d644fe503389f365f9d9c97bac
John Leider <john@vuetifyjs.com>
2024-03-10 20:56:15 -0500

    fix(javascript): swap router files

--- template/javascript/base/src/router/index.js
+++ template/javascript/base/src/router/index.js
@@ -1,3 +1,4 @@
+
 /**
  * router/index.ts
  *
@@ -6,11 +7,9 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  extendRoutes: setupLayouts,
 })
 
 export default router
--- template/javascript/essentials/src/router/index.js
+++ template/javascript/essentials/src/router/index.js
@@ -6,9 +6,11 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  extendRoutes: setupLayouts,
 })
 
 export default router
