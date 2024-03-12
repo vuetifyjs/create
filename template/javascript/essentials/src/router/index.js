786332e0bc871ae63c878300999bf26760dfa7f4
i5NWkv9NH1 <sloananna326@gmail.com>
2024-03-12 19:37:49 +0900

    fix: fix vue-router not working when settings `base` option in vite.config.*

--- template/javascript/essentials/src/router/index.js
+++ template/javascript/essentials/src/router/index.js
@@ -9,7 +9,7 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: setupLayouts,
 })
 
--- template/typescript/base/src/router/index.ts
+++ template/typescript/base/src/router/index.ts
@@ -8,7 +8,7 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
 })
 
 export default router
