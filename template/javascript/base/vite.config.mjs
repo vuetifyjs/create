eb71399470f040306e9599f802dc1ae6e0472e85
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-11-05 17:34:35 -0800

    add host

--- template/javascript/base/vite.config.mjs
+++ template/javascript/base/vite.config.mjs
@@ -49,6 +49,7 @@ export default defineConfig({
     ],
   },
   server: {
+    host: '127.0.0.1',
     port: 3000,
   },
   css: {
--- template/javascript/essentials/vite.config.mjs
+++ template/javascript/essentials/vite.config.mjs
@@ -62,6 +62,7 @@ export default defineConfig({
     ],
   },
   server: {
+    host: '127.0.0.1',
     port: 3000,
   },
   css: {
