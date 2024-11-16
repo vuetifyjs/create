d44e5636e391128309ffd9eec2ec471024fea69f
John Leider <9064066+johnleider@users.noreply.github.com>
2024-11-16 12:30:36 -0600

    feat: update to latest vue install defaults (#62)

---
+++ template/javascript/base/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  }
+]
