20f3c927887c55df5343a776a6d06e8e94ce4964
Andrei Elkin <andreyyolkin@gmail.com>
2025-04-08 17:21:28 +0200

    refactor: silence installation
    To address modern pnpm policy (https://pnpm.io/cli/approve-builds) from poc was backported util to check ignored builds

---
+++ src/utils/cli/pnpmIgnored.ts
@@ -0,0 +1,11 @@
+import { execSync } from 'node:child_process'
+
+export function pnpmIgnored (root: string) {
+  const pnpmVersion = execSync(`cd ${root} && pnpm -v`, { encoding: 'utf8' }).trim()
+  const [major] = pnpmVersion.split('.').map(Number)
+  if (major && major >= 10) {
+    const detect = execSync(`cd ${root} && pnpm ignored-builds`, { encoding: 'utf8' })
+    if (detect.startsWith('Automatically ignored builds during installation:\n  None')) return
+    return detect
+  }
+}
--- src/utils/installDependencies.ts
+++ src/utils/installDependencies.ts
@@ -1,4 +1,5 @@
 import { installDependencies as installDependencies$1 } from 'nypm'
+import { pnpmIgnored } from './cli/pnpmIgnored'
 
 const userAgent = process.env.npm_config_user_agent ?? ''
 
@@ -10,9 +11,17 @@ export async function installDependencies (root: string = process.cwd(), manager
   await installDependencies$1({
     packageManager: manager,
     cwd: root,
-  }).catch(() => {
-    console.error(
-      `Failed to install dependencies using ${manager}.`
-    )
+    silent: true,
   })
+    .catch(() => {
+      console.error(
+        `Failed to install dependencies using ${manager}.`
+      )
+    })
+    .then(() => {
+      if (manager === 'pnpm') {
+        const detect = pnpmIgnored(root)
+        if (detect) console.warn(detect)
+      }
+    })
 }
