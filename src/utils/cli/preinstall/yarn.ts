64dc19a10a6a9e3a4736f03dc4bc91bd68b968c7
Andrei Elkin <andreyyolkin@gmail.com>
2025-04-27 22:06:12 +0200

    fix: add @vue/compiler-sfc as dependency for yarn 2+ (#73)

---
+++ src/utils/cli/preinstall/yarn.ts
@@ -0,0 +1,22 @@
+import { x } from 'tinyexec'
+import { appendFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+const templateToAppend = `
+packageExtensions:
+  unplugin-vue-router@*:
+    dependencies:
+      "@vue/compiler-sfc": "*"
+`
+
+export async function yarnFile (root: string) {
+  const pnpmVersion = (await (x('yarn', ['-v'], { nodeOptions: { cwd: root } }))).stdout.trim()
+  const [major] = pnpmVersion.split('.').map(Number)
+  if (major && major >= 2) {
+    appendFileSync(resolve(root, '.yarnrc.yml'), templateToAppend)
+  }
+}
+
+export default async function yarn (root: string) {
+  await yarnFile(root)
+}
