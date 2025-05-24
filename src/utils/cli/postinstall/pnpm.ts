88a78b4ab6d171fd0a73d55ec033a6ff960b4f42
Andrei Elkin <andreyyolkin@gmail.com>
2025-05-24 17:06:49 +0200

    chore: eslint

--- src/utils/cli/postinstall/pnpm.ts
+++ src/utils/cli/postinstall/pnpm.ts
@@ -5,12 +5,16 @@ export async function pnpmIgnored (root: string) {
   const [major] = pnpmVersion.split('.').map(Number)
   if (major && major >= 10) {
     const detect = (await x('pnpm', ['ignored-builds'], { nodeOptions: { cwd: root } })).stdout
-    if (detect.startsWith('Automatically ignored builds during installation:\n  None')) return
+    if (detect.startsWith('Automatically ignored builds during installation:\n  None')) {
+      return
+    }
     return detect
   }
 }
 
 export default async function pnpm (root: string) {
   const detect = await pnpmIgnored(root)
-  if (detect) console.warn(detect)
+  if (detect) {
+    console.warn(detect)
+  }
 }
