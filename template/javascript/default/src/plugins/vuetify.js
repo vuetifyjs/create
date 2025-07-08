609dc7db9f606bf095797c13eb24a2f2b9531945
Andrei Elkin <andreyyolkin@gmail.com>
2025-07-08 13:32:10 +0200

    feat(theme): change default theme from dark to system

--- template/javascript/default/src/plugins/vuetify.js
+++ template/javascript/default/src/plugins/vuetify.js
@@ -14,6 +14,6 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'system',
   },
 })
