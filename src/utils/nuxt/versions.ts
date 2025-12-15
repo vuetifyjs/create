5294c748b6163ec2e06feffa963b67e524f3f255
J-Sek <J-Sek@users.noreply.github.com>
2025-12-15 15:21:34 +0100

    Lock Sass version to avoid errors

--- src/utils/nuxt/versions.ts
+++ src/utils/nuxt/versions.ts
@@ -2,7 +2,7 @@ export const versions = {
   'vuetify': '^3.9.1',
   'typescript': '^5.8.3',
   'vue-tsc': '^3.0.1',
-  'sass-embedded': '^1.89.2',
+  'sass-embedded': '1.93.3',
   '@vuetify/loader-shared': '^2.1.0',
   'vite-plugin-vuetify': '^2.1.1',
   'vuetify-nuxt-module': '^0.18.7',
