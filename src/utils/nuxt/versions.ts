9cd4d528a911da1789d57996862b62feee176147
Andrei Elkin <andreyyolkin@gmail.com>
2025-12-20 20:27:37 +0100

    chore: update vue-tsc and vuetify-nuxt-module versions
    Update vue-tsc from ^3.0.7 to ^3.2.0 and vuetify-nuxt-module from ^0.18.7 to ^0.19.1 to use latest versions

--- src/utils/nuxt/versions.ts
+++ src/utils/nuxt/versions.ts
@@ -1,11 +1,11 @@
 export const versions = {
   'vuetify': '^3.9.1',
   'typescript': '^5.8.3',
-  'vue-tsc': '^3.0.1',
+  'vue-tsc': '^3.2.0',
   'sass-embedded': '^1.89.2',
   '@vuetify/loader-shared': '^2.1.0',
   'vite-plugin-vuetify': '^2.1.1',
-  'vuetify-nuxt-module': '^0.18.7',
+  'vuetify-nuxt-module': '^0.19.1',
   'upath': '^2.0.1',
   '@mdi/font': '^7.4.47',
   '@nuxt/fonts': '^0.11.4',
